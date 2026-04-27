3a9dfa1447aa055e43acd683bdefa1c87f3411de
PatNoO <Noordh94@hotmail.se>
2026-04-27 21:15:23 +0200

    Initial Commit

---
+++ daily_done/daily_doneApp.swift
@@ -0,0 +1,17 @@
+//
+//  daily_doneApp.swift
+//  daily_done
+//
+//  Created by Patrik Noordh on 2026-04-27.
+//
+
+import SwiftUI
+
+@main
+struct daily_doneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
