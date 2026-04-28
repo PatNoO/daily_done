c2861ee179abd3931dfff012f7d7dc25688bf24c
PatNoO <Noordh94@hotmail.se>
2026-04-28 11:48:22 +0200

    fix [DD-002] Wire Firebase, gitignore plist, remove credential tracking

--- daily_done/daily_doneApp.swift
+++ daily_done/daily_doneApp.swift
@@ -1,7 +1,19 @@
+import FirebaseCore
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 @main
 struct daily_doneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
